c655fd4d8dc1fa0d2a2f757414c696bfcd844d03
Andy DuFrene <andy.dufrene94@gmail.com>
2014-10-04 06:55:01 -0700

    Added push

--- app/src/main/java/io/hackerbros/invite/application/InitApplication.java
+++ app/src/main/java/io/hackerbros/invite/application/InitApplication.java
@@ -1,18 +1,35 @@
 package io.hackerbros.invite.application;
 
 import android.app.Application;
+import android.util.Log;
 
 import io.hackerbros.invite.R;
 
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 public class InitApplication extends Application {
+    private static final String TAG = InitApplication.class.getSimpleName();
 
   @Override
   public void onCreate() {
       super.onCreate();
       Parse.initialize(this, "JM1ObdhIVJuQSh1KogpvZRsU4nvcQkrcutOi9fO7", "s2Zd3ysZ5bL4UO8WDDCrj6djIheWjJHyMRSoYDn0");
       ParseFacebookUtils.initialize(getResources().getString(R.string.app_id));
+
+      ParsePush.subscribeInBackground("", new SaveCallback() {
+          @Override
+          public void done(ParseException e) {
+              if (e != null) {
+                  Log.d(TAG, "successfully subscribed to channel");
+              }
+              else {
+                    Log.d(TAG, "failed to subscribe for push", e);
+              }
+          }
+      });
   }
 }
