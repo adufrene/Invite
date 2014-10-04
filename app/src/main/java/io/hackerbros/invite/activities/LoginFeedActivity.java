1dff6c84390885e1e136fd3a2932c3867d32b2ee
Andy DuFrene <andy.dufrene94@gmail.com>
2014-10-04 03:33:50 -0700

    Added facebook login

--- app/src/main/java/io/hackerbros/invite/activities/LoginFeedActivity.java
+++ app/src/main/java/io/hackerbros/invite/activities/LoginFeedActivity.java
@@ -2,10 +2,13 @@ package io.hackerbros.invite.activities;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.content.Intent;
 
 import io.hackerbros.invite.login.LoginFragment;
+import io.hackerbros.invite.R;
 
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 
 public class LoginFeedActivity extends SimpleFragmentActivity {
   public Fragment getFragment() {
@@ -16,5 +19,12 @@ public class LoginFeedActivity extends SimpleFragmentActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Parse.initialize(this, "JM1ObdhIVJuQSh1KogpvZRsU4nvcQkrcutOi9fO7", "s2Zd3ysZ5bL4UO8WDDCrj6djIheWjJHyMRSoYDn0");
+    ParseFacebookUtils.initialize(getResources().getString(R.string.app_id));
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
   }
 }
