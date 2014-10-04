db9128415f3ae3b65e77bb3d464975cbf0570ce5
Jameson Li <jamesonrli@gmail.com>
2014-10-04 03:40:38 -0700

    adding event

--- app/src/main/java/io/hackerbros/invite/activities/LoginFeedActivity.java
+++ app/src/main/java/io/hackerbros/invite/activities/LoginFeedActivity.java
@@ -2,23 +2,32 @@ package io.hackerbros.invite.activities;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
 
 import com.parse.Parse;
 
 import io.hackerbros.invite.R;
+import io.hackerbros.invite.fragments.AddEventFragment;
 import io.hackerbros.invite.fragments.NewsFeedFragment;
 
-public class LoginFeedActivity extends SimpleFragmentActivity {
-    public Fragment getFragment() {
-        // if not logged in then return LoginFragment.createFragment();
-        return new NewsFeedFragment();
-    }
+public class LoginFeedActivity extends SimpleFragmentActivity implements View.OnClickListener {
+   public Fragment getFragment() {
+      // if not logged in then return LoginFragment.createFragment();
+      return new NewsFeedFragment();
+   }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_feed);
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_login_feed);
 
-        Parse.initialize(this, "JM1ObdhIVJuQSh1KogpvZRsU4nvcQkrcutOi9fO7", "s2Zd3ysZ5bL4UO8WDDCrj6djIheWjJHyMRSoYDn0");
-    }
+      Parse.initialize(this, "JM1ObdhIVJuQSh1KogpvZRsU4nvcQkrcutOi9fO7", "s2Zd3ysZ5bL4UO8WDDCrj6djIheWjJHyMRSoYDn0");
+   }
+
+   @Override
+   public void onClick(View v) {
+      if (v.getId() == R.id.add_event_button) {
+         addFragment(new AddEventFragment());
+      }
+   }
 }
--- app/src/main/java/io/hackerbros/invite/activities/SimpleFragmentActivity.java
+++ app/src/main/java/io/hackerbros/invite/activities/SimpleFragmentActivity.java
@@ -3,25 +3,39 @@ package io.hackerbros.invite.activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import io.hackerbros.invite.R;
 
 public abstract class SimpleFragmentActivity extends Activity {
-  public abstract Fragment getFragment();
+   public abstract Fragment getFragment();
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_fragment);
-    FragmentManager fm = getFragmentManager();
-    Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-     
-    if (fragment == null) {
+   private FragmentManager fm;
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_fragment);
+      fm = getFragmentManager();
+      Fragment fragment; // = fm.findFragmentById(R.id.fragmentContainer);
+
       fragment = getFragment();
       fm.beginTransaction()
-        .add(R.id.login_feed_full_fragment_container, fragment)
-        .commit();
-    }
-  }
+              .add(R.id.login_feed_full_fragment_container, fragment)
+              .commit();
+   }
+
+   public void addFragment(Fragment newFrag) {
+      Fragment newFragment = newFrag;
+      fm.beginTransaction()
+              .add(R.id.login_feed_full_fragment_container, newFragment)
+              .commit();
+   }
+
+   public void removeFragment(Fragment removeFrag) {
+      fm.beginTransaction()
+              .remove(removeFrag)
+              .commit();
+   }
 }
