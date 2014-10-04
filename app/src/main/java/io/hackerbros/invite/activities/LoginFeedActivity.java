a0d90adac8eab6e7e74f7899241ea6a757307c1b
Jameson Li <jamesonrli@gmail.com>
2014-10-04 03:07:39 -0700

    move fragments to fragment folder, initial view for LoginFeedActivity's feed

--- app/src/main/java/io/hackerbros/invite/activities/LoginFeedActivity.java
+++ app/src/main/java/io/hackerbros/invite/activities/LoginFeedActivity.java
@@ -1,9 +1,21 @@
-package io.hackerbros.invite.activities
+package io.hackerbros.invite.activities;
 
 import android.app.Fragment;
+import android.os.Bundle;
+
+import io.hackerbros.invite.R;
+import io.hackerbros.invite.fragments.NewsFeedFragment;
 
 public class LoginFeedActivity extends SimpleFragmentActivity {
-  public Fragment getFragment() {
-    return null;
-  }
+    public Fragment getFragment() {
+        return new NewsFeedFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_feed);
+    }
+
+
 }
---
+++ app/src/main/java/io/hackerbros/invite/fragments/NewsFeedFragment.java
@@ -0,0 +1,33 @@
+package io.hackerbros.invite.fragments;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.hackerbros.invite.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class NewsFeedFragment extends Fragment {
+
+
+    public NewsFeedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_feed, container, false);
+    }
+
+
+}
